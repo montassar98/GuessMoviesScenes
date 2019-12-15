d781096a9c10e69418efbf9f894f38dbec396336
montassar98 <montassarselmi372@gmail.com>
2019-12-15 23:58:59 +0100

    the improving till 12/15. i got video until a specific time and i generate arrays for the buttons

---
+++ app/src/main/java/com/montassar/guessmoviesscenes/Fragments/PlayingVideoFragment.java
@@ -0,0 +1,95 @@
+package com.montassar.guessmoviesscenes.Fragments;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.montassar.guessmoviesscenes.MainActivity;
+import com.montassar.guessmoviesscenes.R;
+
+public class PlayingVideoFragment extends Fragment {
+
+    private VideoView videoView;
+    private int videoId;
+    boolean shouldStop = false;
+    Handler mHandler;
+    Runnable mRunnable;
+
+    public PlayingVideoFragment(int videoId) {
+        this.videoId = videoId;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_playing_video, container, false);
+
+
+        videoView = (VideoView) view.findViewById(R.id.video_view);
+       /* videoView.setVideoPath("file:///android_asset/videos/test.mp4");
+        videoView.start();*/
+        String path = "android.resource://" + getActivity().getPackageName() + "/" + videoId;
+        Toast.makeText(getContext(), "" + R.raw.test, Toast.LENGTH_SHORT).show();
+        // videoView.start();
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                videoView.requestFocus();
+                //videoView.seekTo(MainActivity.videoTime);
+                videoView.start();
+                //
+
+
+            }
+        });
+        videoView.setVideoURI(Uri.parse(path));
+        //progress();
+        trackProgress();
+
+        return view;
+    }
+   /* void progress() {
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            public void run() {
+                int currentPostion = videoView.getCurrentPosition();
+                if(currentPostion >= 10 * 1000 || currentPostion == videoView.getDuration()) {
+                   videoView.stopPlayback();
+                }
+                mHandler.postDelayed(this, 250);
+            }
+        };
+        mHandler.post(mRunnable);
+    }*/
+
+    private void trackProgress() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!shouldStop) {
+                    if (videoView != null && videoView.isPlaying()) {
+                        if (videoView.getCurrentPosition() >= MainActivity.videoTime) {
+                            videoView.stopPlayback();
+                            shouldStop = true;
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+}
--- app/src/main/java/com/montassar/guessmoviesscenes/MainActivity.java
+++ app/src/main/java/com/montassar/guessmoviesscenes/MainActivity.java
@@ -1,16 +1,129 @@
 package com.montassar.guessmoviesscenes;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.montassar.guessmoviesscenes.Fragments.PlayingVideoFragment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String rightAnswer;
+    private ArrayList<HashMap<String,String>> answersList;
+    private ArrayList<String> moviesList;
+     private TextView txtAnswer1,txtAnswer2,txtAnswer3,txtAnswer4,txtAnswer5,txtAnswer6;
+     private HashMap<String,String> answerMap;
+     private final String KEY_ANSWER_GAME="answer";
+     public static int videoTime=10000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //===========/Hide Status Bar/===========
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //=======================================
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "hsin", Toast.LENGTH_SHORT).show();
+        moviesList = new ArrayList<>();
+        answersList = new ArrayList<>();
+        Collections.addAll(moviesList,getResources().getStringArray(R.array.movies));
+
+
+        txtAnswer1 = (TextView) findViewById(R.id.txt_answer_1);
+        txtAnswer2 = (TextView) findViewById(R.id.txt_answer_2);
+        txtAnswer3 = (TextView) findViewById(R.id.txt_answer_3);
+        txtAnswer4 = (TextView) findViewById(R.id.txt_answer_4);
+        txtAnswer5 = (TextView) findViewById(R.id.txt_answer_5);
+        txtAnswer6 = (TextView) findViewById(R.id.txt_answer_6);
+        txtAnswer1.setOnClickListener(this);
+        txtAnswer2.setOnClickListener(this);
+        txtAnswer3.setOnClickListener(this);
+        txtAnswer4.setOnClickListener(this);
+        txtAnswer5.setOnClickListener(this);
+        txtAnswer6.setOnClickListener(this);
+        rightAnswer = "Avengers: End Game";
+
+        generateAnswers(rightAnswer);
+        init(R.raw.test2);
+
+    }
+
+    private void generateAnswers(String rightAnswer) {
+        answerMap = new HashMap<>();
+        answerMap.put(KEY_ANSWER_GAME,rightAnswer);
+        answerMap.put("is_real","1");
+        answersList.add(answerMap);
+        for (int i=0;i<5;i++)
+        {
+            answerMap = new HashMap<>();
+            answerMap.put(KEY_ANSWER_GAME,moviesList.get( new Random().nextInt((15) + 1)));
+            answerMap.put("is_real","0");
+            answersList.add(answerMap);
+        }
+        Collections.shuffle(answersList);
+        txtAnswer1.setText(answersList.get(0).get(KEY_ANSWER_GAME));
+        txtAnswer2.setText(answersList.get(1).get(KEY_ANSWER_GAME));
+        txtAnswer3.setText(answersList.get(2).get(KEY_ANSWER_GAME));
+        txtAnswer4.setText(answersList.get(3).get(KEY_ANSWER_GAME));
+        txtAnswer5.setText(answersList.get(4).get(KEY_ANSWER_GAME));
+        txtAnswer6.setText(answersList.get(5).get(KEY_ANSWER_GAME));
+    }
+
+    private void init(int videoId) {
+
+        PlayingVideoFragment fragment = new PlayingVideoFragment(videoId);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.fragment_container,fragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        String answerClicked="";
+        switch (id)
+        {
+            case R.id.txt_answer_1:
+                answerClicked=txtAnswer1.getText().toString();
+                verifyAnswer(answerClicked);
+                break;
+            case R.id.txt_answer_2:
+                    answerClicked=txtAnswer2.getText().toString();
+                    verifyAnswer(answerClicked);
+                break;
+            case R.id.txt_answer_3:
+                    answerClicked=txtAnswer3.getText().toString();
+                    verifyAnswer(answerClicked);
+               break;
+            case R.id.txt_answer_4:
+                 answerClicked=txtAnswer4.getText().toString();
+                 verifyAnswer(answerClicked);
+                break;
+            case R.id.txt_answer_5:
+                    answerClicked=txtAnswer5.getText().toString();
+                     verifyAnswer(answerClicked);
+               break;
+            case   R.id.txt_answer_6:
+                   answerClicked=txtAnswer6.getText().toString();
+                   verifyAnswer(answerClicked);
+                break;
+               /*
+                break;*/
+        }
+    }
+
+    private void verifyAnswer(String answerClicked) {
+        if (answerClicked==rightAnswer)
+            Toast.makeText(this, "Right Answer", Toast.LENGTH_SHORT).show();
+        else Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show();
     }
 }
